30c8d07d81bc416dafb39e0a9875e0ef84825a7d
CYJ <wjdtpdbs1887@naver.com>
2023-05-30 15:53:16 +0900

    템플릿 오류

--- routes/page.js
+++ routes/page.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { renderProfile, renderJoin, rederMain} = require('../controllers/page');
+const { renderProfile, renderJoin, renderMain} = require('../controllers/page');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
     res.locals.user = null;
     res.locals.followerCount = 0;
-    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
     res.locals.followingIdList = [];
     next();
 });
@@ -15,6 +15,6 @@ router.get('/profile', renderProfile);
 
 router.get('/join', renderJoin);
 
-router.get('/', rederMain);
+router.get('/', renderMain);
 
 module.exports = router;
