3ff646f254a1e1c336898d8184a856bd28d25ec7
CHYIJE <wjdtpdbs1887@naver.com>
2023-06-08 21:51:41 +0900

    팔로우 오류

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,22 @@
 const express = require('express');
 
 const { isLoggedIn } = require('../middlewares');
-const { follow, unfollow } = require('../controllers/user');
+const { follow } = require('../controllers/user');
+
 
 const router = express.Router();
 
 router.post('/:id/follow', isLoggedIn, follow);
-router.post('/:id/unfollow', isLoggedIn, unfollow);
+router.post('/:id/followCancle', isLoggedIn, async(req, res, next) => {
+    try {
+        console.log(req.user.id, req.params.id);
+        const user = await User.findOne({where: { nick: req.params.id } });
+        await user.removeFollower(parseInt(req.user.id));
+        res.send('success');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
 
 module.exports = router;
