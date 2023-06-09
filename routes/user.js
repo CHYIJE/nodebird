ee70168dbf78bbbbe3e7b58be41d7648187b7f43
CYJ <wjdtpdbs1887@naver.com>
2023-06-09 15:59:53 +0900

    팔로우 끊기 완료

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,14 @@
 const express = require('express');
-
 const { isLoggedIn } = require('../middlewares');
 const { follow } = require('../controllers/user');
-
+const { followCancle } = require('../controllers/user');
 
 const router = express.Router();
 
+// Follow a user
 router.post('/:id/follow', isLoggedIn, follow);
-router.post('/:id/followCancle', isLoggedIn, async(req, res, next) => {
-    try {
-        console.log(req.user.id, req.params.id);
-        const user = await User.findOne({where: { nick: req.params.id } });
-        await user.removeFollower(parseInt(req.user.id));
-        res.send('success');
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-});
+
+// Unfollow a user
+router.post('/:id/followCancle', isLoggedIn, followCancle);
 
 module.exports = router;
