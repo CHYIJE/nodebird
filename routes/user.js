8591f17b76b1f754141a35198e8fe53e5ed00e30
CYJ <wjdtpdbs1887@naver.com>
2023-06-09 16:55:48 +0900

    프로필 수정 완료

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { isLoggedIn } = require('../middlewares');
 const { follow } = require('../controllers/user');
 const { followCancle } = require('../controllers/user');
+const { User } = require('../models');
 
 const router = express.Router();
 
@@ -10,5 +11,17 @@ router.post('/:id/follow', isLoggedIn, follow);
 
 // Unfollow a user
 router.post('/:id/followCancle', isLoggedIn, followCancle);
+
+router.patch('/:id', isLoggedIn, async (req, res, next) => {
+    try {
+      console.log(req.user.id, req.params.id);
+      const newNick = req.body.nick;
+      const user = await User.update({ nick: newNick }, { where: { id: req.user.id } });
+      res.send('success');
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  });
 
 module.exports = router;
