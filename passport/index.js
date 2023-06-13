08cd3e40c1126b267171c9d0a8f17dc72e03d753
CYJ <wjdtpdbs1887@naver.com>
2023-06-13 11:45:52 +0900

    프로필편집까지 롤백

--- passport/index.js
+++ passport/index.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const local = require('./localStrategy');
 const kakao = require('./kakaoStrategy');
-const { User, Post } = require('../models');
+const User = require('../models/user');
 
 module.exports = () => {
     passport.serializeUser((user, done) => {
@@ -21,28 +21,10 @@ module.exports = () => {
                 as:'Followings'
             }],
          })
-        .then(user => done(null, user))
-        .catch(err => done(err));
-    })
-
-module.exports = passport => {
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
+            .then(user => done(null, user))
+            .catch(err => done(err));
     });
 
-    passport.deserializeUser((id,done) => {
-        User.findOne({
-            where: { id },
-            include: [
-                { model: User, attributes: ['id', 'nick'], as: 'Followers' },
-                { model: User, attributes: ['id', 'nick'], as: 'Followings' },
-                { model: Post, as: 'LikePosts' },
-            ],
-        })
-        .then(user => done(null, user))
-        .catch(err => done(err));
-    });
-}
-    local(passport);
-    kakao(passport);
+    local();
+    kakao();
 };
