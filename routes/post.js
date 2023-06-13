acd743d8f5c2a10e74e0de7abe7b31d24bcc8280
CHYIJE <wjdtpdbs1887@naver.com>
2023-06-13 16:21:05 +0900

    삭제완료

--- routes/post.js
+++ routes/post.js
@@ -5,32 +5,43 @@ const fs = require('fs');
 
 const { afterUploadImage, uploadPost } = require('../controllers/post');
 const { isLoggedIn } = require('../middlewares');
+const { Post } = require('../models'); // Post 모델을 가져옵니다.
 
 const router = express.Router();
 
-try{
-    fs.readdirSync('uploads');
+try {
+  fs.readdirSync('uploads');
 } catch (error) {
-    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-    fs.mkdirSync('uploads');
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
 }
 
 const upload = multer({
-    storage: multer.diskStorage({
-        destination(req, file, cb) {
-            cb(null, 'uploads/');
-        },
-        filename(req, file, cb) {
-            const ext = path.extname(file.originalname);
-            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-        },
-    }),
-    limits: { fileSize: 5 * 1024 * 1024},
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 router.post('/img', isLoggedIn, upload.single('img'), afterUploadImage);
 
 const upload2 = multer();
 router.post('/', isLoggedIn, upload2.none(), uploadPost);
+
+router.delete('/:postId', isLoggedIn, async (req, res, next) => {
+  try {
+    const post = await Post.destroy({ where: { id: req.params.postId } }); // Post 모델을 사용하여 게시글을 삭제합니다.
+    res.send('success');
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
 
 module.exports = router;
